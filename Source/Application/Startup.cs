d921f8ffec29855fde5af8f60d0f3aeb17c3024c
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2020-03-16 12:19:42 +0100

    Add solution.

---
+++ Source/Application/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Application
+{
+	public class Startup
+	{
+		#region Constructors
+
+		public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment)
+		{
+			this.Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+			this.HostEnvironment = hostEnvironment ?? throw new ArgumentNullException(nameof(hostEnvironment));
+		}
+
+		#endregion
+
+		#region Properties
+
+		protected internal virtual IConfiguration Configuration { get; }
+		protected internal virtual IHostEnvironment HostEnvironment { get; }
+
+		#endregion
+
+		#region Methods
+
+		public virtual void Configure(IApplicationBuilder applicationBuilder)
+		{
+			if(this.HostEnvironment.IsDevelopment())
+			{
+				applicationBuilder.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				applicationBuilder.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				applicationBuilder.UseHsts();
+			}
+
+			applicationBuilder.UseHttpsRedirection();
+			applicationBuilder.UseStaticFiles();
+			applicationBuilder.UseRouting();
+			applicationBuilder.UseAuthorization();
+
+			applicationBuilder.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+		}
+
+		public virtual void ConfigureServices(IServiceCollection services)
+		{
+			if(services == null)
+				throw new ArgumentNullException(nameof(services));
+
+			services.AddControllersWithViews();
+		}
+
+		#endregion
+	}
+}
