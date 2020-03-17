ba9b93dbb489a8958e13508f774be359e328eba7
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2020-03-17 07:45:21 +0100

    Miscellaneous

--- Source/Application/Controllers/HomeController.cs
+++ Source/Application/Controllers/HomeController.cs
@@ -1,43 +1,130 @@
-﻿using System.Diagnostics;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Application.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Application.Controllers
 {
 	public class HomeController : Controller
 	{
-		#region Fields
+		#region Constructors
 
-		private readonly ILogger<HomeController> _logger;
+		public HomeController(IConfiguration configuration)
+		{
+			this.Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+		}
 
 		#endregion
 
-		#region Constructors
+		#region Properties
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+		protected internal virtual IConfiguration Configuration { get; }
 
 		#endregion
 
 		#region Methods
 
+		public virtual async Task<IActionResult> ConfigurationJson()
+		{
+			var jsonObject = new JObject();
+
+			this.PopulateJsonObject(this.Configuration, jsonObject);
+
+			return this.Content(await this.ToJsonAsync(jsonObject));
+		}
+
+		public virtual async Task<IActionResult> ConfigurationProviders()
+		{
+			var jsonObject = new JObject();
+
+			// ReSharper disable InvertIf
+			if(this.Configuration is IConfigurationRoot configurationRoot)
+			{
+				for(var i = 0; i < configurationRoot.Providers.Count(); i++)
+				{
+					var provider = configurationRoot.Providers.ElementAt(i);
+
+					var information = provider.GetType().FullName;
+
+					if(provider is FileConfigurationProvider fileConfigurationProvider)
+						information += " (" + fileConfigurationProvider.Source.Path + ")";
+
+					jsonObject.Add(i.ToString(CultureInfo.InvariantCulture), information);
+				}
+			}
+			// ReSharper restore InvertIf
+
+			return this.Content(await this.ToJsonAsync(jsonObject));
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
+		public virtual async Task<IActionResult> Error()
 		{
-			return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+			return await Task.FromResult(this.View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier}));
 		}
 
-		public IActionResult Index()
+		public virtual async Task<IActionResult> Index()
 		{
-			return View();
+			return await Task.FromResult(this.View());
 		}
 
-		public IActionResult Privacy()
+		protected internal virtual void PopulateJsonObject(IConfiguration configuration, JObject jsonObject)
 		{
-			return View();
+			if(configuration == null)
+				throw new ArgumentNullException(nameof(configuration));
+
+			if(jsonObject == null)
+				throw new ArgumentNullException(nameof(jsonObject));
+
+			foreach(var child in configuration.GetChildren().OrderBy(child => child.Key))
+			{
+				if(child.Exists())
+				{
+					if(child.Value != null)
+					{
+						jsonObject.Add(child.Key, new JValue(child.Value));
+					}
+					else
+					{
+						var childJsonObject = new JObject();
+						this.PopulateJsonObject(child, childJsonObject);
+						jsonObject.Add(child.Key, childJsonObject);
+					}
+				}
+				else
+				{
+					jsonObject.Add(child.Key, null);
+				}
+			}
+		}
+
+		public virtual async Task<IActionResult> Privacy()
+		{
+			return await Task.FromResult(this.View());
+		}
+
+		[SuppressMessage("Style", "IDE0063:Use simple 'using' statement")]
+		protected internal virtual async Task<string> ToJsonAsync(JObject jsonObject)
+		{
+			// ReSharper disable ConvertToUsingDeclaration
+			await using(var stringWriter = new StringWriter())
+			{
+				using(var jsonTextWriter = new JsonTextWriter(stringWriter) {Formatting = Formatting.Indented, Indentation = 1, IndentChar = '\t'})
+				{
+					new JsonSerializer().Serialize(jsonTextWriter, jsonObject);
+
+					return stringWriter.ToString();
+				}
+			}
+			// ReSharper restore ConvertToUsingDeclaration
 		}
 
 		#endregion
--- Source/Application/Program.cs
+++ Source/Application/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Hosting;
 
 namespace Application
@@ -7,9 +9,22 @@ namespace Application
 	{
 		#region Methods
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+		public static IHostBuilder CreateHostBuilder(string[] args)
+		{
+			return Host.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration(configurationBuilder =>
+				{
+					foreach(var configurationSource in configurationBuilder.Sources)
+					{
+						if(!(configurationSource is JsonConfigurationSource jsonConfigurationSource))
+							continue;
+
+						// To get it working in a Linux container where file-names are case-sensitive.
+						jsonConfigurationSource.Path = jsonConfigurationSource.Path.Replace("appsettings", "AppSettings", StringComparison.OrdinalIgnoreCase);
+					}
+				})
+				.ConfigureWebHostDefaults(webHostBuilder => { webHostBuilder.UseStartup<Startup>(); });
+		}
 
 		public static void Main(string[] args)
 		{
