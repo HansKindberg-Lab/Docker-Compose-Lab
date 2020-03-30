1f84661f65dec95805b88ea99adc297c14819aeb
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2020-03-30 10:28:35 +0200

    New certificates.

--- Source/Certificates/Create-Certificates.ps1
+++ Source/Certificates/Create-Certificates.ps1
@@ -12,7 +12,7 @@ New-ClientCertificate `
 	-SignerName $rootCertificateName;
 
 New-SslCertificate `
-	-DnsName "*.azurewebsites.net", "*.company.com", "*.company.net", "*.example.com", "*.example.net", "*.local.net", "localhost", "::1", "127.0.0.1", "127.0.0.2", "127.0.0.3", "127.0.0.4", "127.0.0.5", "127.0.0.6", "127.0.0.7", "127.0.0.8", "127.0.0.9" `
+	-DnsName "*.azurewebsites.net", "*.company.com", "*.company.net", "*.example.com", "*.example.net", "*.hanskindberg.net", "*.local.net", "localhost", "::1", "127.0.0.1", "127.0.0.2", "127.0.0.3", "127.0.0.4", "127.0.0.5", "127.0.0.6", "127.0.0.7", "127.0.0.8", "127.0.0.9" `
 	-Name $sslCertificateName `
 	-SignerName $rootCertificateName;
 
